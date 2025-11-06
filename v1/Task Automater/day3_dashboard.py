3358c9943d38f228d71e22ce57c09dd7e6d81e87
Ramkumar R <ramkumarashvanth09@gmail.com>
2025-11-06 23:29:54 +0530

    Update day3_dashboard.py

--- v1/Task Automater/day3_dashboard.py
+++ v1/Task Automater/day3_dashboard.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 # --- Config ---
 SPREADSHEET_ID = "1GLKQllVmysW4ARvx7RWs2KDQOmsnNMwJWaqebFsMqs4"
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-CREDS = Credentials.from_service_account_file("credentials.json", scopes=SCOPE)
+creds_json = os.getenv("GOOGLE_CREDS")
+CREDS = Credentials.from_service_account_info(json.loads(creds_json))
 client = gspread.authorize(CREDS)
 
 def fetch_data():
@@ -72,3 +73,4 @@ def api_data():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
+
